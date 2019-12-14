c470bd581ad8e35d2a9b37b375f2b3b2205e9bf8
Twixxik <43586827+Twixxik@users.noreply.github.com>
2019-12-14 14:36:27 +0300

    Start

---
+++ gulp-tasks/serve.js
@@ -0,0 +1,23 @@
+"use strict";
+
+import { paths } from "../gulpfile.babel";
+import gulp from "gulp";
+import browsersync from "browser-sync";
+
+gulp.task("serve", () => {
+    browsersync.init({
+        server: "./dist/",
+        proxy: 'twixxik.dev',
+        port: 4000,
+        notify: false,
+        https: true,
+        tunnel: "twixxik",
+        online: true,
+    });
+
+    gulp.watch(paths.views.watch, gulp.parallel("views"));
+    gulp.watch(paths.styles.watch, gulp.parallel("styles"));
+    gulp.watch(paths.scripts.watch, gulp.parallel("scripts"));
+    gulp.watch(paths.sprites.watch, gulp.parallel("sprites"));
+    gulp.watch(paths.images.watch, gulp.parallel("images"));
+});
